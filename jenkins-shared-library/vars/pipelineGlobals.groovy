5d61f9e8d37564225e5864f8c0c1b9ca73144908
Shashi <shashikanth0312@gmail.com>
2026-07-02 10:41:09 +0530

    adding

---
+++ jenkins-shared-library/vars/pipelineGlobals.groovy
@@ -0,0 +1,11 @@
+static final String region(){
+    return "us-east-1"
+}
+
+static final String nexusUrl(){
+    return "db-dev.daws78s.online"
+}
+
+static final String account_id(){
+    return "23435473545"
+}
